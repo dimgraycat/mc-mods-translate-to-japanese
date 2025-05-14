8032c38a513807a2b1c1232674db318666e913bb
dimgraycat <dimgray.cat@gmail.com>
2025-05-14 21:12:08 +0900

    add mod names

--- src/config/modnames.php
+++ src/config/modnames.php
@@ -2,5 +2,11 @@
 
 return [
     'pamhc2crops' => "Pam's HarvestCraft 2 - Crops",
+    'pamhc2foodcore' => "Pam's HarvestCraft 2 - Food Core",
+    'pamhc2foodextended' => "Pam's HarvestCraft 2 - Food Extended",
+    'pamhc2trees' => "Pam's HarvestCraft 2 - Trees",
+    'displaydelight' => "Display Delight",
+    'reimaginingpotatoes' => "Poisonous Potato Update",
+    'twilightforest' => 'The Twilight Forest (黄昏の森)',
     'enchant-levels' => "Enchantment Level Romanizer",
 ];
