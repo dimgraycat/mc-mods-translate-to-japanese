91cfd0fa34fa2d21d9f72ada3f8bf31a98396d32
dimgraycat <dimgray.cat@gmail.com>
2025-05-14 19:47:22 +0900

    update list-json

--- src/config/modnames.php
+++ src/config/modnames.php
@@ -1,3 +1,5 @@
+<?php
+
 return [
     'pamhc2crops' => "Pam's HarvestCraft 2 - Crops",
     'enchant-levels' => "Enchantment Level Romanizer",
--- src/config/resources.php
+++ src/config/resources.php
@@ -1,3 +1,5 @@
+<?php
+
 return [
     'base_url' => 'https://github.com/dimgraycat/mc-mods-translate-to-japanese/releases/download/v1.0.0'
 ];
